9cfcd1c60f530981f64a5bd26e0b99a94a2f74ec
Jinhwan0515 <jinhwan0515@gamil.com>
2020-06-03 14:11:11 +0900

    song search frontend add

---
+++ src/main/resources/static/res/js/search.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+	$('button#button-search').click(function () {
+		search();
+	});
+
+	$("#keyword").keydown(function (e) {
+		if (e.keyCode == 13){
+			e.prenentDefault();
+			search();
+		}
+	});
+});
+
+function search() {
+
+	let searchType = $('.container #searchTypeSelect option:selected').val();
+	let keyword = $('#keyword').val();
+	$.ajax({
+		url: `/rest/song/search/${searchType}?${searchType}=${keyword}`,
+		success: function (data) {
+			$('#searchResult > tbody').html('');
+			console.log(data);
+			if (!data.empty) {
+				let content = data.content;
+
+				content.forEach(song => {
+					$('#searchResult > tbody').append(`<tr><td>${song.number}</td><td>${song.name}</td><td>${song.singer}</td><td>${song.lyricist}</td><td>${song.composer}</td></tr>`);
+				});
+			}
+		}
+	});
+
+	return false;
+}
+
